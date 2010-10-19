03b332ae83cee113dd0cbb52934265060c2b6892
Mayeutik <mayeu.tik@gmail.com>
2010-10-19 10:41:37 +0200

    Add src/zap.c file za# Untracked files:

---
+++ src/zap.c
@@ -0,0 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <mayeu.tik@gmail.com> and <pierrealain.toret@gmail.com> wrote this file. As
+ * long as you retain this notice you can do whatever you want with this stuff.
+ * If we meet some day, and you think this stuff is worth it, you can buy us a
+ * beer in return. Matthieu Maury & Pierre-Alain Toret
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * @file zap.c
+ * @brief Implement a little command line utility that use libzap
+ * @author <mayeu.tik@gmail.com> <pierrealain.toret@gmail.com>
+ * @date 2010/10/19
+ */
+
+
