fabd20e506709e299bfb781c43a46bc6c54135d8
Mayeutik <mayeu.tik@gmail.com>
2010-10-25 19:24:21 +0200

    Add bool.h with the definition of the boolean type

---
+++ src/include/bool.h
@@ -0,0 +1,30 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <mayeu.tik@gmail.com> and <pierrealain.toret@gmail.com> wrote this file. As
+ * long as you retain this notice you can do whatever you want with this stuff.
+ * If we meet some day, and you think this stuff is worth it, you can buy us a
+ * beer in return. Matthieu Maury & Pierre-Alain Toret
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * @file zops.c
+ * @brief Implement functions to add, double, inverse, and multiplication of a point
+ * @author <mayeu.tik@gmail.com> <pierrealain.toret@gmail.com>
+ * @date 2010/10/13
+ */
+
+/*
+ * Define
+ */
+
+#ifndef __BOOL_H
+#define __BOOL_H
+
+typedef enum {
+    false,
+    true
+} bool;
+
+#endif                          /* __BOOL_H */
