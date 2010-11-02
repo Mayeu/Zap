cc7344afdfadd0f076a1b082f5e1bfe8a20ee1d3
daftaupe <pierrealain.toret@gmail.com>
2010-11-02 16:18:42 +0100

    Add Diffie-Hellman protocol header file

---
+++ src/include/dh.h
@@ -0,0 +1,39 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <mayeu.tik@gmail.com> and <pierrealain.toret@gmail.com> wrote this file. As
+ * long as you retain this notice you can do whatever you want with this stuff.
+ * If we meet some day, and you think this stuff is worth it, you can buy us a
+ * beer in return. Matthieu Maury & Pierre-Alain Toret
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * @file dh.h
+ * @brief Define the functions of the Diffie-Hellman key exchange
+ * @author <mayeu.tik@gmail.com> <pierrealain.toret@gmail.com>
+ * @date 2010/10/14
+ */
+
+#ifndef __DH_H
+#define __DH_H
+
+/*
+ * Include
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <gmp.h>
+#include <bool.h>
+
+/*
+ * Define
+ */
+
+/*
+ * Functions
+ */
+
+mpz_t
+#endif                          /* __DH_H */
