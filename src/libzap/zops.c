b701630009b91010e620bc6c0886a29b8cd0522b
Mayeutik <mayeu.tik@gmail.com>
2010-11-21 02:16:50 +0100

    Correct all kind of bug in zops.c

--- src/libzap/zops.c
+++ src/libzap/zops.c
@@ -50,7 +50,8 @@ void            add_pts(ecpts_t * R, ecpts_t * P, ecpts_t * Q);
 void
 zadd(ecpts_t * R, ecpts_t * P, ecpts_t * Q)
 {
-    ecpts_t        *tmp;
+    ecpts_t        *tmp,
+                   *pt;
 
     /*
      * P is the point at infinity
@@ -91,20 +92,26 @@ zadd(ecpts_t * R, ecpts_t * P, ecpts_t * Q)
 
     ecpts_destroy(tmp);
 
+    pt = ecpts_init();
+
     /*
      * P and Q are equals
      * We return the double of P
      */
     if (ecpts_are_equals(P, Q)) {
-        zdouble(R, P);
+        zdouble(pt, P);
+        ecpts_cpy(R, pt);
+        ecpts_destroy(pt);      /* Free */
         return;
     }
 
     /*
      * addition
      */
-    add_pts(R, P, Q);
+    add_pts(pt, P, Q);
+    ecpts_cpy(R, pt);
 
+    ecpts_destroy(pt);
     return;
 }
 
@@ -119,22 +126,34 @@ void
 zinvert(ecpts_t * R, ecpts_t * P)
 {
     eccrvw_t       *C;
+    ecpts_t        *pt;
 
+    pt = ecpts_init();
     C = P->C;
 
     /*
      * xr
      */
-    mpz_set(R->x, P->x);
+    mpz_set(pt->x, P->x);
 
     /*
      * yr
      */
-    mpz_ui_sub(R->y, 0, P->y);
-    mpz_mod(R->y, R->y, C->p);
+    mpz_ui_sub(pt->y, 0, P->y);
+    mpz_mod(pt->y, pt->y, C->p);
 
-    ecpts_set_inf(R, P->inf);
-    ecpts_set_curve(R, P->C);
+    /*
+     * Misc
+     */
+    ecpts_set_inf(pt, P->inf);
+    ecpts_set_curve(pt, P->C);
+
+    ecpts_cpy(R, pt);
+
+    /*
+     * Free the memory
+     */
+    ecpts_destroy(pt);
 
     return;
 }
@@ -152,8 +171,10 @@ zdouble(ecpts_t * R, ecpts_t * P)
     mpz_t           lmbd,
                     lmbd2,
                     tmp;
+    ecpts_t        *pt;
     eccrvw_t       *C;
 
+    pt = ecpts_init();
     C = P->C;
 
     /*
@@ -163,43 +184,46 @@ zdouble(ecpts_t * R, ecpts_t * P)
 
     mpz_mul(lmbd, P->x, P->x);  /* lmbd = xp^2 */
     mpz_mul_ui(lmbd, lmbd, 3);  /* lmbd = 3xp^2 */
-    // mpz_mul(tmp, P->x, C->a2); /* tmp = a2*xp */
-    // mpz_mul_ui(tmp, tmp, 2); /* tmp = 2a2*xp */
-    // mpz_add(lmbd, lmbd, tmp); /* lmbd = 3xp^2 + 2a2*xp */
-    mpz_add(lmbd, lmbd, C->a4); /* lmbd = 3xp^2 + 2a2*xp + a4 */
-    // mpz_mul(tmp, C->a1, P->y); /* tmp = a1*py */
-    // mpz_sub(lmbd, lmbd, tmp); /* lmbd = 3xp^2 + 2a2*xp + a4 - a1*py */
+    mpz_add(lmbd, lmbd, C->a4); /* lmbd = 3xp^2 + a4 */
     mpz_add(lmbd2, P->y, P->y); /* lmbd2 = 2yp */
-    // mpz_mul(tmp, C->a1, P->x); /* tmp = a1*xp */
-    // mpz_add(lmbd2, lmbd2, tmp); /* lmbd2 = yp^2 + a1*xp */
-    // mpz_add(lmbd2, lmbd2, C->a3); /* lmbd2 = yp^2 + a1*yp + a3 */
-    mpz_invert(lmbd2, lmbd2, C->p);     /* lmbd2 = lmbd2^-1 */
-    mpz_mul(lmbd, lmbd, lmbd2); /* lmbd = (3xp^2 + 2a2*xp + a4 - a1*py) /
-                                 * (yp^2 + a1*yp + a3) */
+    mpz_invert(lmbd2, lmbd2, C->p);     /* lmbd2 = 2yp^-1 */
+    mpz_mul(lmbd, lmbd, lmbd2); /* lmbd = (3xp^2 + a4) / yp^2 */
     mpz_mod(lmbd, lmbd, C->p);
 
     /*
      * xr
      */
-    mpz_mul(R->x, lmbd, lmbd);  /* xr = lmbd^2 */
-    // mpz_mul(tmp, lmbd, C->a1); /* tmp = a1*lmbd */
-    // mpz_add(R->x, R->x, tmp); /* xr = lmbd^2 + a1*lmbd */
-    // mpz_sub(R->x, R->x, a2); /* xr = lmbd^2 + a1*lmbd - a2 */
-    mpz_sub(R->x, R->x, P->x);  /* xr = lmbd^2 + a1*lmbd - a2 - xp */
-    mpz_mod(R->x, R->x, C->p);  /* stay in the additive ring ! */
+    mpz_mul(pt->x, lmbd, lmbd); /* xr = lmbd^2 */
+    mpz_mul_ui(tmp, P->x, 2);   /* tmp = 2xp */
+    mpz_sub(pt->x, pt->x, tmp); /* xr = lmbd^2 - 2xp */
+    mpz_mod(pt->x, pt->x, C->p);        /* stay in the additive ring ! */
 
     /*
      * yr
      */
-    // mpz_add(R->y, lmbd, C->a1); /* yr = lmbd + a1 */
-    mpz_mul_si(R->y, lmbd, -1); /* yr = -(lmbd + a1) */
-    mpz_mul(R->y, R->y, R->x);  /* yr = -(lmbd + a1)xr */
+    mpz_neg(pt->y, lmbd);       /* yr = -lmbd */
+    mpz_mul(pt->y, pt->y, pt->x);       /* yr = -lmbd*xr */
     mpz_mul(tmp, lmbd, P->x);   /* tmp = lmbd*xp */
-    mpz_add(R->y, R->y, tmp);   /* yr = -(lmbd + a1)xr + lmbd*xp */
-    mpz_sub(R->y, R->y, P->y);  /* yr = -(lmbd + a1)xr + lmbd*xp - yp */
-    mpz_mod(R->y, R->y, C->p);  /* stay in the additive ring ! */
+    mpz_add(pt->y, pt->y, tmp); /* yr = -lmbd*xr + lmbd*xp */
+    mpz_sub(pt->y, pt->y, P->y);        /* yr = -lmbd*xr + lmbd*xp - yp */
+    mpz_mod(pt->y, pt->y, C->p);        /* stay in the additive ring ! */
 
+    /*
+     * Misc part
+     */
+    ecpts_set_inf(pt, P->inf);
+    ecpts_set_curve(pt, P->C);
+
+    /*
+     * Copy the result in R
+     */
+    ecpts_cpy(R, pt);
+
+    /*
+     * Free the memory
+     */
     mpz_clears(lmbd, lmbd2, tmp, NULL);
+    ecpts_destroy(pt);
 
     return;
 }
@@ -216,9 +240,19 @@ void
 zmult(ecpts_t * R, ecpts_t * P, mpz_t k)
 {
     char           *nb;
+
     nb = NULL;
     nb = mpz_get_str(nb, 2, k); /* we convert k into a string of bit */
+
     ecpts_cpy(R, P);
+
+    /*
+     * if k = 0, we return the point at infinity
+     */
+    if (mpz_cmp_ui(k, 0) == 0) {
+        ecpts_set_inf(R, true);
+        return;
+    }
 
     /*
      * if P is the point at infinity, there is nothing to do 
@@ -226,16 +260,18 @@ zmult(ecpts_t * R, ecpts_t * P, mpz_t k)
     if (ecpts_is_inf(P))
         return;
 
+    nb = NULL;
+
+    nb = mpz_get_str(nb, 2, k); /* we convert k into a string of bit */
+
     /*
      * we start at n-1 bit
      */
-    nb++;
-    while (*nb != '\0') {       // modif by pat
-        zadd(R, R, R);
-        if (*nb == '1') {
+    while (*(++nb) != '\0') {
+        zdouble(R, R);
+        if (*nb == '1')
             zadd(R, R, P);
         }
-        nb++;
     }
     return;
 }
@@ -255,48 +291,56 @@ zmult(ecpts_t * R, ecpts_t * P, mpz_t k)
 void
 add_pts(ecpts_t * R, ecpts_t * P, ecpts_t * Q)
 {
-    mpz_t           X,
-                    lmbd,
+    mpz_t           lmbd,
                     tmp;
+    ecpts_t        *pt;
     eccrvw_t       *C;          /* the curve of the points */
 
+    pt = ecpts_init();
     C = P->C;
 
-    mpz_inits(X, lmbd, tmp, NULL);
-
-    /*
-     * X
-     */
-    mpz_add(X, P->x, Q->x);     /* X = xp + xq */
+    mpz_inits(lmbd, tmp, NULL);
 
     /*
      * lmbd
      */
-    mpz_add(lmbd, P->y, Q->y);  /* lmbd = yp + yq */
-    mpz_invert(tmp, X, C->p);   /* tmp = X^-1 */
-    mpz_mul(lmbd, lmbd, X);     /* lmbd = (yp + yq) * X^-1 */
+    mpz_sub(lmbd, Q->y, P->y);  /* lmbd = yq - yp */
+    mpz_sub(tmp, Q->x, P->x);   /* tmp = xq - xp */
+    mpz_invert(tmp, tmp, C->p); /* tmp = (xq - xp)^-1 */
+    mpz_mul(lmbd, lmbd, tmp);   /* lmbd = (yq - yp) / (xq - xp) */
+    mpz_mod(lmbd, lmbd, C->p);  /* Stay in your division ring */
 
     /*
      * xr
      */
-    mpz_mul(R->x, lmbd, lmbd);  /* xr = lmbd^2 */
-    mpz_add(R->x, R->x, lmbd);  /* xr = lmbd^2 + lmbd */
-    mpz_add(R->x, R->x, X);     /* xr = lmbd^2 + lmbd + X */
-    mpz_mod(R->x, R->x, C->p);  /* Stay in your division ring ! */
+    mpz_mul(pt->x, lmbd, lmbd); /* xr = lmbd^2 */
+    mpz_sub(pt->x, pt->x, P->x);        /* xr = lmbd^2 - xp */
+    mpz_sub(pt->x, pt->x, Q->x);        /* xr = lmbd^2 - xp - xq */
+    mpz_mod(pt->x, pt->x, C->p);        /* Stay in your division ring ! */
 
     /*
      * yr
      */
-    mpz_add_ui(R->y, lmbd, 1);  /* yr = (lmbd + 1) */
-    mpz_mul(R->y, R->y, R->x);  /* yr = (lmbd + 1) * xr */
-    mpz_mul(lmbd, lmbd, P->x);  /* lmbd = lmbd * xp ; we can * use lmbd to 
-                                 * temp the value * since it will not be
-                                 * reuse. */
-    mpz_add(R->y, R->y, lmbd);  /* yr = (lmbd + 1) * xr + * lmbd*xp */
-    mpz_add(R->y, R->y, P->y);  /* yr = (lmbd + 1) * xr + lmbd*xp + yp */
-    mpz_mod(R->y, R->y, C->p);  /* Stay in your division ring ! */
+    mpz_neg(pt->y, lmbd);       /* yr = -lmbd */
+    mpz_mul(pt->y, pt->y, pt->x);       /* yr = -lmbd * xr */
+    mpz_mul(tmp, lmbd, P->x);   /* tmp = lmbd * xp */
+    mpz_add(pt->y, pt->y, tmp); /* yr = -lmbd * xr + lmbd*xp */
+    mpz_sub(pt->y, pt->y, P->y);        /* yr = -lmbd * xr + lmbd*xp - yp */
+    mpz_mod(pt->y, pt->y, C->p);        /* Stay in your division ring ! */
 
-    mpz_clears(X, lmbd, tmp, NULL);
+    /*
+     * Misc
+     */
+    ecpts_set_inf(pt, P->inf);
+    ecpts_set_curve(pt, P->C);
+
+    ecpts_cpy(R, pt);
+
+    /*
+     * Free the memory
+     */
+    mpz_clears(lmbd, tmp, NULL);
+    ecpts_destroy(pt);
 
     return;
 }
